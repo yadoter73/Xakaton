4faa1eaf978bf1b575292c3d0d5089579be2f3a4
yadoter73 <tomanovpl@gmail.com>
2026-04-23 11:22:30 +0400

    1
    1

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -1,44 +1,104 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class FirstPersonMovement : MonoBehaviour
 {
-    public float speed = 5;
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 7f;
 
-    [Header("Running")]
+    [Header("Stamina")]
     public bool canRun = true;
-    public bool IsRunning { get; private set; }
-    public float runSpeed = 9;
+    [SerializeField] private float maxStamina = 5f;
+    [SerializeField] private float staminaDrainPerSecond = 1f;
+    [SerializeField] private float staminaRegenPerSecond = 0.75f;
+    [SerializeField] private float staminaRequiredToRun = 0.5f;
+    [SerializeField] private float startingStamina = -1f;
+
+    private float staminaRegenDelay = 3f;
+
+    [Header("Controls")]
     public KeyCode runningKey = KeyCode.LeftShift;
 
-    Rigidbody rigidbody;
-    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public bool IsRunning { get; private set; }
+    public float currentStamina;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private float currentRegenDelayTimer;
+
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
-
-
 
     void Awake()
     {
-        // Get the rigidbody on this.
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        currentStamina = (startingStamina < 0f) ? maxStamina : Mathf.Clamp(startingStamina, 0f, maxStamina);
+    }
+
+    void Update()
+    {
+
+        HandleInputAndStamina();
     }
 
     void FixedUpdate()
     {
-        // Update IsRunning from input.
-        IsRunning = canRun && Input.GetKey(runningKey);
+        ApplyMovement();
+    }
 
-        // Get targetMovingSpeed.
-        float targetMovingSpeed = IsRunning ? runSpeed : speed;
-        if (speedOverrides.Count > 0)
+    private void HandleInputAndStamina()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        bool isMoving = moveInput.sqrMagnitude > 0.01f;
+        bool inputRun = Input.GetKey(runningKey);
+
+        if (IsRunning)
         {
-            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+            if (!inputRun || currentStamina <= 0 || !isMoving)
+            {
+                IsRunning = false;
+            }
+        }
+        else
+        {
+            if (canRun && inputRun && currentStamina >= staminaRequiredToRun && isMoving)
+            {
+                IsRunning = true;
+            }
         }
 
-        // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        if (IsRunning)
+        {
+            currentStamina -= staminaDrainPerSecond * Time.deltaTime;
+            currentRegenDelayTimer = staminaRegenDelay; 
+        }
+        else
+        {
+            if (currentRegenDelayTimer > 0)
+            {
+                currentRegenDelayTimer -= Time.deltaTime;
+            }
+            else
+            {
+                currentStamina += staminaRegenPerSecond * Time.deltaTime;
+            }
+        }
+        currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+    }
 
-        // Apply movement.
-        rigidbody.linearVelocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.linearVelocity.y, targetVelocity.y);
+    private void ApplyMovement()
+    {
+        float targetSpeed = IsRunning ? runSpeed : walkSpeed;
+
+        if (speedOverrides.Count > 0)
+        {
+            targetSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+        Vector3 moveDirection = (transform.right * moveInput.x + transform.forward * moveInput.y).normalized;
+        Vector3 targetVelocity = moveDirection * targetSpeed;
+
+        rb.linearVelocity = new Vector3(targetVelocity.x, rb.linearVelocity.y, targetVelocity.z);
     }
 }
