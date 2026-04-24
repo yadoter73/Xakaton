6d739f102dd4e3222b1d8454affe5abd6fdc1db8
yadoter73 <tomanovpl@gmail.com>
2026-04-24 18:17:18 +0400

    1
    1

--- Assets/PROXOR/Scripts/InteractableOBJ/DoorBehaviour.cs
+++ Assets/PROXOR/Scripts/InteractableOBJ/DoorBehaviour.cs
@@ -23,7 +23,10 @@ public class DoorBehaviour : MonoBehaviour, IInteractable
         _isOpen = !_isOpen;
         _animator.SetBool("IsOpen", _isOpen);
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
     public string GetDescription()
     {
         return _isOpen ? "Press E to open" : "Press E to close";
