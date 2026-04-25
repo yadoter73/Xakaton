bfa4be03e1b99f51fc9d47ab47f3d931fc8a2982
yadoter73 <tomanovpl@gmail.com>
2026-04-26 03:50:36 +0400

    1
    1

--- Assets/PROXOR/Scripts/InteractableOBJ/BottleFly.cs
+++ Assets/PROXOR/Scripts/InteractableOBJ/BottleFly.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,11 +7,13 @@ public class BottleFly : MonoBehaviour, IInteractable
     [SerializeField] private GameObject _bottlePrefab;
     [SerializeField] Image _hand;
     [SerializeField] Sprite _bottle;
+    [SerializeField] TextMeshProUGUI _text;
     public string GetDescription() => "Бери!!!";
 
     public void Interact(int interactionType)
     {
-
+        _text.gameObject.SetActive(true);
+        _text.text = "(ЛКМ)Брось в охранника СПЕРЕДИ!!!";
         ThrowController thrower = FindObjectOfType<ThrowController>();
         thrower.Pickup(_bottlePrefab);
         _hand.sprite = _bottle;
