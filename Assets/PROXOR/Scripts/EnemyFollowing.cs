08739852113b6223e3082a57adc8a511b65b8860
yadoter73 <tomanovpl@gmail.com>
2026-04-25 21:02:23 +0400

    1
    1

--- Assets/PROXOR/Scripts/EnemyFollowing.cs
+++ Assets/PROXOR/Scripts/EnemyFollowing.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 using UnityEngine.AI;
-
+using PrimeTween;
 public class EnemyFollowing : MonoBehaviour
 {
     [SerializeField] GameObject _target;
     private NavMeshAgent _agent;
+
+    [SerializeField] private string obstacleTag = "Obstacle"; 
+    [SerializeField] private float pushForce = 5f;
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
     }
-    public void Following()
+    private void Update()
     {
-        _agent.SetDestination(_target.transform.position);
+        if (_target != null)
+        {
+            _agent.SetDestination(_target.transform.position);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(obstacleTag))
+        {
+            if (!collision.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                rb = collision.gameObject.AddComponent<Rigidbody>();
+            }
+
+            Vector3 pushDirection = collision.transform.position - transform.position;
+            pushDirection.y = 0;
+            pushDirection = pushDirection.normalized;
+
+            rb.AddForce(pushDirection * pushForce, ForceMode.Impulse);
+        }
     }
 }
+
